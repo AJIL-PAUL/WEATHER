0a373fd8de9b7778c18efa7f1ac695fe4dd3ea90
AJIL-PAUL <ajilpaul.268@gmail.com>
2019-12-27 10:19:15 +0530

    Updates

---
+++ precache-manifest.0978d9e952427752ca22dc264c96b6d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "279c31b667bf2ccc06e82de609e5ee0d",
+    "url": "/WEATHER/index.html"
+  },
+  {
+    "revision": "269da90e7bfd6e495d61",
+    "url": "/WEATHER/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "2abbde297426d1090199",
+    "url": "/WEATHER/static/css/main.cda86000.chunk.css"
+  },
+  {
+    "revision": "269da90e7bfd6e495d61",
+    "url": "/WEATHER/static/js/2.5b6ab929.chunk.js"
+  },
+  {
+    "revision": "27650def0d6865cedb2014229f52ad5f",
+    "url": "/WEATHER/static/js/2.5b6ab929.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2abbde297426d1090199",
+    "url": "/WEATHER/static/js/main.4af5177a.chunk.js"
+  },
+  {
+    "revision": "a8eb4a1cb3df575f99c3",
+    "url": "/WEATHER/static/js/runtime-main.c27d916e.js"
+  }
+]);
